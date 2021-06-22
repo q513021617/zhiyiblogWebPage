e6fa23e6342692444cfddf138e444fdb215dfb9c
john <1871901600@qq.com>
2021-06-22 14:24:20 +0800

    feat: 项目PWA化

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './registerServiceWorker'
 
 Vue.use(BootstrapVue)
 
--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ module.exports = {
   devServer: {
     proxy: { //配置代理，解决跨域请求后台数据的问题
       '/blogapi/': {
-        target: 'http://127.0.0.1:7888/',
-        // target: 'http://www.zhiyigo.cn/blogapi/', //后台接口
+        // target: 'http://127.0.0.1:7888/',
+        target: 'http://www.zhiyigo.cn/blogapi/', //后台接口
         ws: true, //是否跨域
         changeOrigin: true,
         pathRewrite: {
@@ -18,7 +18,30 @@ module.exports = {
       }
     }
   },
-
+  pwa: {
+    workboxPluginMode: 'GenerateSW', // 也可以定义为‘InjectManifest’模式。但是需自己写SW.js文件进行配置
+    workboxOptions: {
+        importWorkboxFrom: 'cdn', //从''cdn"导入workbox,也可以‘local’
+        skipWaiting: true, // 安装完SW不等待直接接管网站
+        clientsClaim: true,
+        navigateFallback: '/index.html', 
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/], //在预缓存中排除图片
+        // 定义运行时缓存
+        runtimeCaching: [
+            {
+                urlPattern: new RegExp('^https://cdn'),
+                handler: 'NetworkFirst',
+                options: {
+                    networkTimeoutSeconds: 20,
+                    cacheName: 'cdn-cache',
+                    cacheableResponse: {
+                        statuses: [200]
+                    }
+                }
+            }
+        ]
+    }
+},
     configureWebpack: {
         plugins: [
             new webpack.ProvidePlugin({
